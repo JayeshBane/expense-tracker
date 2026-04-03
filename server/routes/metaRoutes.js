35478e68aa992d76ad55dcd77e6c76802799e36b
Jayesh Bane <jayeshvbane@gmail.com>
2026-04-03 14:41:47 -0400

    inject meta controller functions in routes

--- server/routes/metaRoutes.js
+++ server/routes/metaRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const { getMeta } = require("../controllers/metaController");
+const {
+  getCategories,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getPaymentMethods,
+  createPaymentMethod,
+  updatePaymentMethod,
+  deletePaymentMethod,
+} = require("../controllers/metaController");
 
 const router = express.Router();
 
-router.get("/", getMeta);
+router.get("/categories", getCategories);
+router.post("/categories", createCategory);
+router.put("/categories/:id", updateCategory);
+router.delete("/categories/:id", deleteCategory);
+
+router.get("/payment-methods", getPaymentMethods);
+router.post("/payment-methods", createPaymentMethod);
+router.put("/payment-methods/:id", updatePaymentMethod);
+router.delete("/payment-methods/:id", deletePaymentMethod);
 
 module.exports = router;
