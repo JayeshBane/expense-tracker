9f79607d81d945ed975dc84e038b948a5b5f20d5
Jayesh Bane <jayeshvbane@gmail.com>
2026-04-09 21:44:13 -0400

    cretae fetch and create expenses store methods

--- client/src/stores/useExpenseStore.js
+++ client/src/stores/useExpenseStore.js
@@ -1,3 +1,86 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import * as api from "../services/api";
+
+const groupByDate = (expenses) => {
+  expenses.reduce((acc, exp) => {
+    const key = exp.expenseDate.slice(0, 10);
+
+    if (!acc[key]) acc[key] = [];
+
+    acc[key].push(exp);
+
+    return acc;
+  }, {});
+};
+
+export const useExpenseStore = create(
+  devtools((set, get) => ({
+    expenses: [],
+    expensesByDate: {},
+    currentMonth: new Date(),
+    filters: {
+      start: null,
+      end: null,
+      categoryId: null,
+      paymentMethodId: null,
+    },
+    isLoading: false,
+    error: null,
+
+    setCurrentMonth: (date) => set({ currentMonth: date }),
+
+    setFilter: (key, value) =>
+      set((s) => ({ filters: { ...s.filters, [key]: value } })),
+
+    clearFilters: () =>
+      set({
+        filters: {
+          start: null,
+          end: null,
+          categoryId: null,
+          paymentMethodId: null,
+        },
+      }),
+
+    fetchExpenses: async (overrides = {}) => {
+      set({ isLoading: true, error: null });
+
+      try {
+        const params = { ...get().filters, ...overrides };
+        const expenses = await api.getExpenses(params);
+
+        set({
+          expenses,
+          expensesByDate: groupByDate(expenses),
+          isLoading: false,
+        });
+      } catch (err) {
+        set({ error: err.message, isLoading: false });
+      }
+    },
+
+    createExpense: async (data) => {
+      try {
+        const expense = await api.createExpense(data);
+
+        set((s) => {
+          const key = expense.expenseDate.slice(0, 10);
+
+          return {
+            expenses: [expense, ...s.expenses],
+            expensesByDate: {
+              ...s.expensesByDate,
+              [key]: [...(s.expensesByDate[key] ?? []), expense],
+            },
+          };
+        });
+
+        return expense;
+      } catch (err) {
+        set({ error: err.message });
+        return null;
+      }
+    },
+  })),
+);
