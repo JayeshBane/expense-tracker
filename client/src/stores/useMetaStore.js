114d481798afebd2d997a8c90accaa962b4ce8a2
Jayesh Bane <jayeshvbane@gmail.com>
2026-04-09 21:23:47 -0400

    complete meta store

--- client/src/stores/useMetaStore.js
+++ client/src/stores/useMetaStore.js
@@ -3,65 +3,70 @@ import { devtools } from "zustand/middleware";
 import * as api from "../services/api";
 
 export const useMetaStore = create(
-  devtools((set) => ({
-    categories: [],
-    paymentMethods: [],
-    isCreating: false,
-    createError: null,
+  devtools(
+    (set) => ({
+      categories: [],
+      paymentMethods: [],
+      isCreating: false,
+      createError: null,
 
-    fetchMeta: async () => {
-      const [categories, paymentMethods] = await Promise.all([
-        api.getCategories(),
-        api.getPaymentMethods(),
-      ]);
+      fetchMeta: async () => {
+        const [categories, paymentMethods] = await Promise.all([
+          api.getCategories(),
+          api.getPaymentMethods(),
+        ]);
 
-      set({ categories, paymentMethods });
-    },
+        set({ categories, paymentMethods });
+      },
 
-    createCategory: async ({ name, color }) => {
-      set({ isCreating: true, createError: null });
+      createCategory: async ({ name, color }) => {
+        set({ isCreating: true, createError: null });
 
-      try {
-        const created = await api.createCategory({ name, color });
-        set((s) => ({
-          categories: [...s.categories, created].sort((a, b) =>
-            a.name.localCompare(b.name),
-          ),
-          isCreating: false,
-        }));
+        try {
+          const created = await api.createCategory({ name, color });
+          set((s) => ({
+            categories: [...s.categories, created].sort((a, b) =>
+              a.name.localCompare(b.name),
+            ),
+            isCreating: false,
+          }));
 
-        return created;
-      } catch (err) {
-        set({
-          createError: err.response?.data?.error ?? "Failed",
-          isCreating: false,
-        });
+          return created;
+        } catch (err) {
+          set({
+            createError: err.response?.data?.error ?? "Failed",
+            isCreating: false,
+          });
 
-        return null;
-      }
-    },
+          return null;
+        }
+      },
 
-    createPaymentMethod: async ({ name }) => {
-      set({ isCreating: true, createError: null });
+      createPaymentMethod: async ({ name }) => {
+        set({ isCreating: true, createError: null });
 
-      try {
-        const created = await api.createPaymentMethod({ name });
-        set((s) => ({
-          paymentMethods: [...s.paymentMethods, created].sort((a, b) =>
-            a.name.localeCompare(b.name),
-          ),
-          isCreating: false,
-        }));
+        try {
+          const created = await api.createPaymentMethod({ name });
+          set((s) => ({
+            paymentMethods: [...s.paymentMethods, created].sort((a, b) =>
+              a.name.localeCompare(b.name),
+            ),
+            isCreating: false,
+          }));
 
-        return created;
-      } catch (err) {
-        set({
-          createError: err.response?.data?.error ?? "Failed",
-          isCreating: false,
-        });
+          return created;
+        } catch (err) {
+          set({
+            createError: err.response?.data?.error ?? "Failed",
+            isCreating: false,
+          });
 
-        return null;
-      }
-    },
-  })),
+          return null;
+        }
+      },
+
+      clearCreateError: () => set({ createError: null }),
+    }),
+    { name: "MetaStore" },
+  ),
 );
