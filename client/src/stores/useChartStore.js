019ad3da31d66c7962c3f7abba7797e664399186
Jayesh Bane <jayeshvbane@gmail.com>
2026-04-09 22:02:25 -0400

    create state defaults and getDateRange

--- client/src/stores/useChartStore.js
+++ client/src/stores/useChartStore.js
@@ -8,3 +8,34 @@ import {
   addMonths,
 } from "date-fns";
 import * as api from "../services/api";
+
+export const useChartStore = create(
+  devtools((set, get) => ({
+    barData: [],
+    lineData: [],
+    pieData: [],
+    isLoading: false,
+
+    // Range controls for bar and pie charts
+    rangeMode: "month",
+    selectedMonth: new Date(),
+    customStart: null,
+    customEnd: null,
+
+    // Number of months for line chart X axis
+    lineMonths: 6,
+
+    getDateRange: () => {
+      const { rangeMode, selectedMonth, customStart, customEnd } = get();
+
+      if (rangeMode === "month") {
+        return {
+          start: format(startOfMonth(selectedMonth), "yyyy-MM-dd"),
+          end: format(endOfMonth(selectedMonth), "yyyy-MM-dd"),
+        };
+      }
+
+      return { start: customStart, end: customEnd };
+    },
+  })),
+);
